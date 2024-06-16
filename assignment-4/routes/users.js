3e72a49952ff54b0e7148df904e4c1e21b1d354a
Jean Fellipe <jeanfellipesilva93@gmail.com>
2024-06-16 20:20:08 -0300

    add user route and includes

--- assignment-4/routes/users.js
+++ assignment-4/routes/users.js
@@ -4,8 +4,15 @@ const express = require("express")
 
 const router = express.Router()
 
+const users = []
+
 router.get("/", (req, res, next) => {
     res.render("users")
+})
+
+router.post("/", (req, res, next) => {
+    users.push(req.body.username)
+    res.redirect("/users")
 })
 
 module.exports = router
